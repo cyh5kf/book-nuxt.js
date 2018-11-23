8c9985b57e676a8ebced56d9520ea2135255f0e0
chenyu <chenyu@mchz.com.cn>
2018-11-23 10:27:52 +0800

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
     title: pkg.name,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
@@ -69,7 +69,6 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
-    vendor: ['axios', 'element-ui'],
     babel: {        //配置按需引入规则
       "plugins":[
           [
